e41d5fbd2182b942c4af83e215a639f1ed5acc7d
Vedant Jawake <vedantjawake6@gmail.com>
2026-08-20 23:15:32 +0530

    Initial Spring Boot backend setup

---
+++ govnext-backend/src/main/java/com/govnext/backend/GovnextBackendApplication.java
@@ -0,0 +1,13 @@
+package com.govnext.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovnextBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovnextBackendApplication.class, args);
+	}
+
+}
---
+++ govnext-backend/src/test/java/com/govnext/backend/GovnextBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govnext.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovnextBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
