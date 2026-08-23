6e41a90713ca3e68223825b4fa5b287e27914554
Vedant Jawake <vedantjawake6@gmail.com>
2026-08-23 17:10:22 +0530

    UserDto class are created

---
+++ govnext-backend/src/main/java/com/govnext/backend/dto/UserDto.java
@@ -0,0 +1,39 @@
+package com.govnext.backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UserDto {
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Must be a valid email address")
+    private String email;
+
+    public UserDto() {}
+
+    public UserDto(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() { 
+        return name; 
+    }
+    
+    public void setName(String name) { 
+        this.name = name; 
+    }
+
+    public String getEmail() { 
+        return email; 
+    }
+    
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
+}
