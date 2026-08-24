961e70be6fee78c6742933dd5155d0ae539f6c78
Vedant Jawake <vedantjawake6@gmail.com>
2026-08-24 20:39:17 +0530

    updated

--- govnext-backend/src/main/java/com/govnext/backend/GovnextBackendApplication.java
+++ govnext-backend/src/main/java/com/govnext/backend/GovnextBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GovnextBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GovnextBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GovnextBackendApplication.class, args);
+    }
 
 }
